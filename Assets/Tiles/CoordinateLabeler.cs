d329633c8618604ce70748cbac48cf700d89c535
onrsvnc <onrsvnc@hotmail.com>
2022-09-28 16:29:59 +0300

    Enemy Pathfinding & Build Timer
    -Enemy pathing reworked with Breadth First Search.
    -Build Timer added to cannons.

--- Assets/Tiles/CoordinateLabeler.cs
+++ Assets/Tiles/CoordinateLabeler.cs
@@ -10,17 +10,20 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f);
 
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
+   
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = true;  //Change this before launch
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
     void Update()
@@ -32,36 +35,55 @@ public class CoordinateLabeler : MonoBehaviour
         }
 
         SetLabelColor();
-        ToggleLabels();
+        // ToggleLabels();
     }
 
-    void ToggleLabels()
-    {
-        if(Input.GetKey(KeyCode.C))
-        {
-            label.enabled = true;
-        }
-        else
-        {
-            label.enabled = false;
-        }
-    }
+    // void ToggleLabels()
+    // {
+    //     if(Input.GetKey(KeyCode.C))
+    //     {
+    //         label.enabled = true;
+    //     }
+    //     else
+    //     {
+    //         label.enabled = false;
+    //     }
+    // }
     void SetLabelColor()
     {
-        if(waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        }
-        else
+        if(gridManager == null) {return;}
+        
+        Node node = gridManager.GetNode(coordinates);
+
+        if(node == null) {return;}
+
+        if(!node.isWalkable)
         {
             label.color = blockedColor;
         }
+
+        else if(node.isPath)
+        {
+            label.color = pathColor;
+        }
+
+        else if(node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+
+        else
+        {
+            label.color = defaultColor;
+        }
+
     }
 
     void DisplayCoordinates()
     {
-        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
-        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+        if(gridManager == null) { return; }
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / gridManager.UnityGridSize);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / gridManager.UnityGridSize);
 
         label.text = coordinates.x + "," + coordinates.y;
     }
--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 500;
+    [SerializeField] float buildDelay = 1;
+
+    void Start()
+    {
+        StartCoroutine(Build());
+    }
 
     public bool CreateTower(Tower tower, Vector3 position)
     {
@@ -23,5 +29,30 @@ public class Tower : MonoBehaviour
         }
         
         return false;   
+    }
+
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        transform.GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.SetActive(true);
+        yield return new WaitForSeconds(buildDelay);
+        transform.GetChild(0).GetChild(5).gameObject.SetActive(true);
+        transform.GetChild(0).GetChild(4).gameObject.SetActive(true);
+        transform.GetChild(0).GetChild(3).gameObject.SetActive(true);
+        transform.GetChild(0).GetChild(2).gameObject.SetActive(true);
+        yield return new WaitForSeconds(buildDelay);
+        transform.GetChild(0).GetChild(6).gameObject.SetActive(true);
+        yield return new WaitForSeconds(buildDelay);
+        transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+        transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
     }
 }
